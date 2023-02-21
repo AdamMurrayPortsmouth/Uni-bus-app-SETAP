f29a5b93f2ef552f4eb4143013fc6077a5cc2826
Adam-Murray <adam.m.murray@kcl.ac.uk>
2023-02-21 18:00:33 +0000

    Added the data loaded to the map
    Added the data loaded to the map however this uses test data as the database and its loader are yet to be created

--- app/lib/Map/open_layers_map.dart
+++ app/lib/Map/open_layers_map.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 
 import 'package:app/Map/user_icon_enum.dart';
+import 'package:app/MapData/map_data_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:path/path.dart';
 
 import 'package:webview_flutter/webview_flutter.dart';
 
+import '../MapData/bus_stop.dart';
+import '../MapData/data.dart';
+import '../MapData/map_data.dart';
 import '../Permissions/location_permissions_handler.dart';
 
 class OpenLayersMap
@@ -34,7 +38,10 @@ class OpenLayersMap
           },
           onPageStarted: (String url) {},
           onPageFinished: (String url) {
-            _addMarkers();
+            MapDataLoader.getDataLoader().onDataLoaded((mapData)
+            {
+              _addMarkers(mapData);
+            });
             _addUserLocationIcon();
           },
           onWebResourceError: (WebResourceError error) {},
@@ -58,11 +65,38 @@ class OpenLayersMap
     }
   }
 
-  _addMarkers()
+  _addMarkers(MapData mapData)
   {
-    // TODO: Use Actual data
-    //String jsObject = "{id: $id, longitude: $long, latitude: $lat}";
-    //webViewController.runJavaScript("addBusStopMarker($jsObject)");
+    for(BusStop busStop in mapData.getBusStops())
+    {
+      _addBusStopMarker(busStop.id, busStop.long, busStop.lat);
+    }
+    for(Data uniBuilding in mapData.getUniBuildings())
+    {
+      _addUniBuildingMarker(uniBuilding.id, uniBuilding.long, uniBuilding.lat);
+    }
+    for(Data landmark in mapData.getLandmarks())
+    {
+      _addLandmarkMarker(landmark.id, landmark.long, landmark.lat);
+    }
+  }
+
+  _addBusStopMarker(String id, double long, double lat)
+  {
+    String jsObject = "{id: '$id', longitude: $long, latitude: $lat}";
+    webViewController.runJavaScript("addBusStopMarker($jsObject)");
+  }
+
+  _addUniBuildingMarker(String id, double long, double lat)
+  {
+    String jsObject = "{id: '$id', longitude: $long, latitude: $lat}";
+    webViewController.runJavaScript("addUniBuildingMarker($jsObject)");
+  }
+
+  _addLandmarkMarker(String id, double long, double lat)
+  {
+    String jsObject = "{id: '$id', longitude: $long, latitude: $lat}";
+    webViewController.runJavaScript("addLandmarkMarker($jsObject)");
   }
 
   _markerClicked(String markerId)
---
+++ app/lib/MapData/data.dart
@@ -0,0 +1,9 @@
+class Data
+{
+  final String id;
+  final String name;
+  final double long;
+  final double lat;
+
+  Data(this. id, this.name, this.long, this.lat);
+}
