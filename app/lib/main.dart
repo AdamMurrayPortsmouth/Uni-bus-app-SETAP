cc4bd3f25c27a49f2843eea1fe7fe20326c9d2c6
Adam-Murray <adam.m.murray@kcl.ac.uk>
2023-02-22 10:19:18 +0000

    Refactored my app
    Put MyApp back into main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-import 'Screen/my_app.dart';
+import 'MapData/map_data_loader.dart';
+import 'Permissions/location_permissions_handler.dart';
+import 'Screen/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    MapDataLoader.getDataLoader().load();
+    LocationPermissionsHandler.getHandler().requestLocationPermission();
+    return const MaterialApp(
+        home: MainScreen()
+  }
 }
