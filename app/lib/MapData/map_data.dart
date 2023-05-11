e1ddcc983ce42da4a3bfedb6b78d91d1d2c1f3f0
up2041097 <up2041097@myport.ac.uk>
2023-05-11 13:40:27 +0100

    remove of test data

--- app/lib/MapData/map_data.dart
+++ app/lib/MapData/map_data.dart
@@ -15,9 +15,9 @@ class MapData
     _uniBuildings = <Feature>{};
     _landmarks = <Feature>{};
     // TODO: remove this test data
-    _busStops.add(BusStop("Bus stop ID", "Bus stop Name", -1.0832318, 50.7937047));
-    _uniBuildings.add(Feature("Uni building ID", "Uni building Name", -1.0882318, 50.7937047));
-    _landmarks.add(Feature("Landmark ID", "Landmark Name", -1.0932318, 50.7937047));
+   // _busStops.add(BusStop("Bus stop ID", "Bus stop Name", -1.0832318, 50.7937047));
+   // _uniBuildings.add(Feature("Uni building ID", "Uni building Name", -1.0882318, 50.7937047));
+   // _landmarks.add(Feature("Landmark ID", "Landmark Name", -1.0932318, 50.7937047));
   }
 
   /// Returns the set of bus stops.
