ec6b22745887b8943e6bbf5d23ac05bf026b9887
UP2042436 <91568903+UP2042436@users.noreply.github.com>
2023-05-11 10:42:48 +0100

    Map Screen
    Map Screen completed.

--- app/lib/Screen/map_screen_state.dart
+++ app/lib/Screen/map_screen_state.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import 'main_screen.dart';
 
@@ -24,17 +23,18 @@ class MapScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: Color(0xffffffff),
       appBar:
       AppBar(
         elevation:4,
         centerTitle:false,
         automaticallyImplyLeading: false,
-        backgroundColor:Color(0xff3a57e8),
+        backgroundColor:Color(0xffa93ae8),
         shape:RoundedRectangleBorder(
           borderRadius:BorderRadius.zero,
         ),
         title:Text(
-          "AppBar",
+          "Map Screen",
           style:TextStyle(
             fontWeight:FontWeight.w400,
             fontStyle:FontStyle.normal,
@@ -48,53 +48,172 @@ class MapScreenState extends State<MainScreen> {
           size:24,
         ),
       ),
-      body:
-      Column(
-        mainAxisAlignment:MainAxisAlignment.start,
-        crossAxisAlignment:CrossAxisAlignment.center,
-        mainAxisSize:MainAxisSize.max,
-        children: [
-          Container(
-            margin:EdgeInsets.zero,
-            padding:EdgeInsets.zero,
-            width:200,
-            height:100,
-            decoration: BoxDecoration(
-              color:Color(0x1f000000),
-              shape:BoxShape.rectangle,
-              borderRadius:BorderRadius.zero,
-              border:Border.all(color:Color(0x4d9e9e9e),width:1),
-            ),
-            child: GoogleMap(
-              mapType: MapType.hybrid,
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            ), // Map
-          )
-
-          ,
-          MaterialButton(
-            onPressed:(){},
-            color:Color(0xffffffff),
-            elevation:0,
-            shape:RoundedRectangleBorder(
-              borderRadius:BorderRadius.zero,
-              side:BorderSide(color:Color(0xff808080),width:1),
-            ),
-            padding:EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child:Text("Text Button", style: TextStyle( fontSize:14,
-              fontWeight:FontWeight.w400,
-              fontStyle:FontStyle.normal,
-            ),),
-            textColor:Color(0xff000000),
-            height:40,
-            minWidth:140,
+      body:Container(
+        margin:EdgeInsets.all(0),
+        padding:EdgeInsets.all(0),
+        width:500,
+        height:2400,
+        decoration: BoxDecoration(
+          color:Color(0x1f000000),
+          shape:BoxShape.rectangle,
+          borderRadius:BorderRadius.zero,
+          border:Border.all(color:Color(0x4d9e9e9e),width:1),
+        ),
+        child:
+        Container(
+          margin:EdgeInsets.zero,
+          padding:EdgeInsets.zero,
+          width:200,
+          height:100,
+          decoration: BoxDecoration(
+            color:Color(0x1f000000),
+            shape:BoxShape.rectangle,
+            borderRadius:BorderRadius.zero,
+            border:Border.all(color:Color(0x4d9e9e9e),width:1),
           ),
-        ],),
-    )
-    ;
+          child:
 
-  }
+          Column(
+            mainAxisAlignment:MainAxisAlignment.start,
+            crossAxisAlignment:CrossAxisAlignment.center,
+            mainAxisSize:MainAxisSize.max,
+            children: [
+              TextField(
+                controller:TextEditingController(),
+                obscureText:false,
+                textAlign:TextAlign.start,
+                maxLines:1,
+                style:TextStyle(
+                  fontWeight:FontWeight.w400,
+                  fontStyle:FontStyle.normal,
+                  fontSize:14,
+                  color:Color(0xff000000),
+                ),
+                decoration:InputDecoration(
+                  disabledBorder:OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(4.0),
+                    borderSide:BorderSide(
+                        color:Color(0xff000000),
+                        width:1
+                    ),
+                  ),
+                  focusedBorder:OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(4.0),
+                    borderSide:BorderSide(
+                        color:Color(0xff000000),
+                        width:1
+                    ),
+                  ),
+                  enabledBorder:OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(4.0),
+                    borderSide:BorderSide(
+                        color:Color(0xff000000),
+                        width:1
+                    ),
+                  ),
+                  hintText:"From:Location",
+                  hintStyle:TextStyle(
+                    fontWeight:FontWeight.w400,
+                    fontStyle:FontStyle.normal,
+                    fontSize:14,
+                    color:Color(0xff000000),
+                  ),
+                  filled:true,
+                  fillColor:Color(0xab700c7d),
+                  isDense:false,
+                  contentPadding:EdgeInsets.fromLTRB(12, 8, 12, 8),
+                ),
+              ),
+              TextField(
+                controller:TextEditingController(),
+                obscureText:false,
+                textAlign:TextAlign.start,
+                maxLines:1,
+                style:TextStyle(
+                  fontWeight:FontWeight.w400,
+                  fontStyle:FontStyle.normal,
+                  fontSize:14,
+                  color:Color(0xff000000),
+                ),
+                decoration:InputDecoration(
+                  disabledBorder:OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(4.0),
+                    borderSide:BorderSide(
+                        color:Color(0xff000000),
+                        width:1
+                    ),
+                  ),
+                  focusedBorder:OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(4.0),
+                    borderSide:BorderSide(
+                        color:Color(0xff000000),
+                        width:1
+                    ),
+                  ),
+                  enabledBorder:OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(4.0),
+                    borderSide:BorderSide(
+                        color:Color(0xff000000),
+                        width:1
+                    ),
+                  ),
+                  hintText:"To:Location",
+                  hintStyle:TextStyle(
+                    fontWeight:FontWeight.w400,
+                    fontStyle:FontStyle.normal,
+                    fontSize:14,
+                    color:Color(0xff000000),
+                  ),
+                  filled:true,
+                  fillColor:Color(0xab700c7d),
+                  isDense:false,
+                  contentPadding:EdgeInsets.fromLTRB(12, 8, 12, 8),
+                ),
+              ),
+              Container(
+                margin:EdgeInsets.zero,
+                padding:EdgeInsets.zero,
+                width:500,
+                height:590,
+                decoration: BoxDecoration(
+                  color:Color(0xb4ebee02),
+                  shape:BoxShape.rectangle,
+                  borderRadius:BorderRadius.zero,
+                  border:Border.all(color:Color(0x4d9e9e9e),width:1),
+                ),
+                child: GoogleMap(
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: _kGooglePlex,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                ),
+              )
+
+              ,
+              Align(
+                alignment:Alignment(-0.1, 0.0),
+                child:MaterialButton(
+                  onPressed:(){},
+                  color:Color(0xab700c7d),
+                  elevation:0,
+                  shape:RoundedRectangleBorder(
+                    borderRadius:BorderRadius.zero,
+                    side:BorderSide(color:Color(0xff808080),width:1),
+                  ),
+                  padding:EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child:Text("Home", style: TextStyle( fontSize:14,
+                    fontWeight:FontWeight.w400,
+                    fontStyle:FontStyle.normal,
+                  ),),
+                  textColor:Color(0xff000000),
+                  height:90,
+                  minWidth:410,
+                ),
+              ),
+            ],),
+        ),
+      ),
+    )
+    ;}
 }
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,9 +6,57 @@ import 'Screen/main_screen.dart';
 
 /// Target for start of application.
 void main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bus Application'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open Map'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MyApp()),
+
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
 
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
 /// The entire application is started from this class.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
